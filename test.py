06c4c1db5ad8c7af7d79718b46a5faca836f5f62
buffed57 <46943749+buffed57@users.noreply.github.com>
2019-01-24 15:00:03 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import re
@@ -16,8 +16,8 @@ soup = BeautifulSoup(html.text,"html.parser")
  # rip it out
 
 # get text
-#text = soup.get_text()
-#print(text)
+text = soup.get_text()
+print(text)
 """# break into lines and remove leading and trailing space on each
 lines = (line.strip() for line in text.splitlines())
 # break multi-headlines into a line each
@@ -33,36 +33,52 @@ stop_words = stopwords.words('english')
 #We create a list comprehension which only returns a list of words #that are NOT IN stop_words and NOT IN punctuations.
 keywords = [word for word in tokens if not word in stop_words and not word in punctuations]
 """
+def getStatementNames(text):
+    pass
 
-#newtext = re.findall(r'[A-Za-z]+|\d+', text)
 
-test_text = 'estimation.44Table of ContentsAlphabet Inc.ITEM 8.FINANCIAL STATEMENTS AND SUPPLEMENTARY DATAAlphabet Inc.INDEX TO CONSOLIDATED FINANCI'
+test_text = 'estimation.44Table of ContentsAlphabet Inc.ITEM 8.FINANCIAL STATEMENTS AND SUPPLEMENTARY DATA45Item 9.Alphabet Inc.INDEX TO CONSOLIDATED FINANCI FINANCIAL STATEMENTS AND SUPPLEMENTARY DATA'
 Item_8_H = 'FINANCIAL STATEMENTS AND SUPPLEMENTARY DATA'
 Item_8_L = 'Financial Statements and Supplementary Data'
 item_8_len = len(Item_8_H)
 phrase_builder = ''
-count = 0
-for letter in test_text:
+phrase_count = 0
+string_count = 0
+
+for letter in text:
+    string_count += 1
     #print(letter)
-    print(phrase_builder)
-    if len(phrase_builder) == item_8_len:
-        if phrase_builder == Item_8_H or phrase_builder == Item_8_L:
-            print(phrase_builder)
-            break
-        else:
-            for x in phrase_builder:
-                count += 1
-                print(x)
-                if x.isalpha() is False:
-                    phrase_builder = phrase_builder[count:]
-                    count = 0
-                    break
+    #print(string_count)
+    #print(phrase_builder)
     if letter.isalpha() or letter.isdigit() is False:
-
         phrase_builder += letter
-
     else:
         phrase_builder = ''
+    if len(phrase_builder) == item_8_len:
+        if phrase_builder == Item_8_H or phrase_builder == Item_8_L:
+            #print(string_count)
+            #print(phrase_builder)
+            #tester_text = text[string_count:string_count+20]
+            #print(tester_text)
+            newtext = re.findall(r'[A-Za-z]+|\d+', text[string_count:string_count+100])
+            print(newtext)
+            if 'Consolidated' not in newtext or 'CONSOLIDATED' not in newtext:
+                print("Wrong set")
+                phrase_builder = ''
+                phrase_count = 0
+            else:
+                print("Success")
+                print(phrase_builder)
+                print(string_count)
+                print(text[string_count:])
 
 
+        else:
+            for x in phrase_builder:
+                phrase_count += 1
+                #print(x)
+                if x.isalpha() is False:
+                    phrase_builder = phrase_builder[phrase_count:]
+                    phrase_count = 0
+                    break
 
